ded221c28d6169e5a9036a6f1560cbba0d224d9a
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-09-22 20:14:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ // Get the user's browser name and version
+        const browserName = navigator.appName;
+        const version = navigator.appVersion;
+
+        // Create a message to display
+        const message = "You are using " + browserName + " version " + version;
+
+        // Find the div element with the id "browser-info" and set its text content
+        const browserInfoDiv = document.getElementById("browser-info");
+        browserInfoDiv.textContent = message;
